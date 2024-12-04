1156f319b7a14507ac5053c887a3edccd04f09c2
Justin <justinguerrerowork@gmail.com>
2024-12-04 13:52:00 -0500

    about and animated title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0);
 
-    const totalVideos = 3;
+    const totalVideos = 4;
 
     const nextVideoRef = useRef(null);
 
@@ -138,7 +138,7 @@ const Hero = () => {
             <div className = "absolute left-0 top-0 z-40 size-full">
                 <div className = "mt-24 px-5 sm:px-10">
                     <h1 className = "special-font hero-heading text-blue-100">
-                        redef<b>n</b>e
+                        redefi<b>n</b>e
                     </h1>
                     <p className = "mb-5 max-w-64 font-robert-regular text-blue-100">
                         Enter the Metagame Layer <br/> Unleash The Play Economy
